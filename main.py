23a1f4fa36c45ed3772e072b570931fbf1fedff7
Vinxkumar <vinxkumar178@gmail.com>
2026-01-30 16:11:43 +0530

    calling UI from main

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.UI.ui import mainWindow
+
+def runUI():
+    mainwindow = mainWindow()
+    mainwindow.runMainWindowUI()
+
+    # JD = Orchestratory().getJD(sample_requirements)
+    # Orchestratory().postToTelegram(JD)
+if __name__ == "__main__":
+    runUI()

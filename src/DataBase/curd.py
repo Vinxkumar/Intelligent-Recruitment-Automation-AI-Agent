a29823d88ee9b4e09bf78a86d832fe92fc04c4a4
Vinxkumar <vinxkumar178@gmail.com>
2026-01-30 19:18:09 +0530

    added 3 new functions one to insert into drive table and another to update the drive status finally to delete drive when no longer needed

--- src/DataBase/curd.py
+++ src/DataBase/curd.py
@@ -12,6 +12,18 @@ class CURD:
         self.cur.execute(queries.create_table_drive)
         self.connect.commit()
 
+    def insert_into_table_drive(self, val:tuple):
+        self.cur.execute(queries.insert_into_drive, val)
+        self.connect.commit()
+
+    def update_table_drive(self, drive_name, drive_status):
+        sql = queries.update_table_drive_status.format(drive_name=drive_name, drive_status = drive_status)
+        self.cur.execute(sql)
+        self.connect.commit()
+
+
+
+
     def create_table_recruitment(self, table_name: str):
         sql_query = queries.create_table_recruitment.format(table_name = table_name)
         self.cur.execute(sql_query)

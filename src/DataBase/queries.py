b0a8addfa84122e05e59853967c9a5c4340d47d9
Vinxkumar <vinxkumar178@gmail.com>
2026-01-30 19:07:05 +0530

    New Queries added and some changes in the existing queries

--- src/DataBase/queries.py
+++ src/DataBase/queries.py
@@ -5,15 +5,15 @@ create table if not exists drive (
     drive_name varchar2(25) not null,
     drive_status boolean not null default true,
     created_at = timestamp default current_timestamp
-)
+);
 """
 
 insert_into_drive = """
-    insert into drive (drive_name, drive_status) values (%s, %s)
+    insert into drive (drive_name, drive_status) values (%s, %s);
 """
 
-alter_table_drive_status = """
-alter table drive 
+update_table_drive_status = """
+update drive set drive_status = {drive_status} where drive_name = {drive_name};
 """
 
 create_table_recruitment = """
@@ -22,9 +22,9 @@ create table if not exists {table_name} (
     name varchar(15) not null,
     email varchar(25) not null unique,
     resume_link varchar(2048) not null
-)
+);
 """
 
 insert_into_recruitment = """
-insert into {table_name} (id, name, email, resume_link) values (%s, %s, %s, %s)
+insert into {table_name} (id, name, email, resume_link) values (%s, %s, %s, %s);
 """

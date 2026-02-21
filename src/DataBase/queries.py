933d1c6af457569dc38a7d180c032bd468491076
Vinxkumar <vinxkumar178@gmail.com>
2026-02-21 20:24:02 +0530

    updated create_requirement query  and added 2 new query one to insert into 'drive_name_Candidates' table and another to get the status of the drive_name

--- src/DataBase/queries.py
+++ src/DataBase/queries.py
@@ -59,3 +59,7 @@ create table if not exists {table_name}_Candidates (
 insert_into_recruitment = """
 insert into {table_name}_Candidates (id, name, lname, phone, email, dob, address, pincode, gender, resume_link) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
 """
+
+check_Drive_status = """
+select drive_status from drive where drive_name = %s;
+"""

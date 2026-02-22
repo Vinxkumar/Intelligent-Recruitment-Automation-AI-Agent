096f6d6c98b8e2ac1d765945c1481b23be883363
Vinxkumar <vinxkumar178@gmail.com>
2026-02-22 17:42:47 +0530

    Created a new query to select candiates from database

--- src/DataBase/queries.py
+++ src/DataBase/queries.py
@@ -60,6 +60,10 @@ insert_into_recruitment = """
 insert into {table_name}_Candidates (id, name, lname, phone, email, dob, address, pincode, gender, resume_link) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
 """
 
+select_recruitment = """
+    select id, name, lname, phone, email, resume_link from {table_name}_Candidates;
+"""
+
 check_Drive_status = """
 select drive_status from drive where drive_name = %s;
 """

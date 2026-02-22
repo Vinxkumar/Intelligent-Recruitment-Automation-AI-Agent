800b411dfabc09e5db71e5b8e850267f8f79d171
Vinxkumar <vinxkumar178@gmail.com>
2026-02-22 15:52:50 +0530

    Added a new column to display Applicants

--- src/FrontEnd/static/js/index_script.js
+++ src/FrontEnd/static/js/index_script.js
@@ -1,4 +1,4 @@
-alert("Hello, World! This is a test alert from index_script.js.");
+// alert("Hello, World! This is a test alert from index_script.js.");
 
 function displayDriveDetails() {
     const driveDetailsDiv = document.getElementById("driveDetails");
@@ -18,7 +18,7 @@ function displayDriveDetails() {
                                     width: 300px;
                                     height: 40px;
                                     font-size: 25px; 
-                                    font-family: consolas;
+                                    font-family: 'Consolas';
                                     color:white; 
                                     padding: 5px; 
                                     margin: 10px 10px 10px 10px;
@@ -33,13 +33,16 @@ function displayDriveDetails() {
                                     width: 120px;
                                     height: 40px;
                                     font-size: 25px; 
-                                    font-family: consolas;
+                                    font-family: 'Consolas';
                                     color:white; 
                                     padding: 5px; 
                                     margin: 10px 10px 10px 10px;
                                 ">
                                     <strong>${drive.drive_status}</strong>
                                 </label>
+                            </td>
+                            <td>
+                                <button name="${drive.drive_name}" id="btn" onclick="listCandidates()">View Candidates</button>
                             </td>
                             <td>
                                 <button name="${drive.drive_name}" id="btn" onclick="handleDrive()">Initiate / Disable</button>
@@ -61,6 +64,10 @@ function handleDrive() {
     const driveName = document.getElementById("btn").getAttribute("name");
     alert(`You clicked on drive: ${driveName}`);
 
+}
+function listCandidates() {
+    const driveName = document.getElementById("btn").getAttribute("name");
+    alert(`You want to view candidates for drive: ${driveName}`);
 }
 
 window.onload = displayDriveDetails;

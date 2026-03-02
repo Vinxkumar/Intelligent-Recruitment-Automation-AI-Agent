549e6050e24f8727d31a262f14e1fe090ae0f1e3
Vinxkumar <vinxkumar178@gmail.com>
2026-03-02 11:23:42 +0530

    [ Update ]: Polised the promt to give intended output.

--- src/Prompts/prompts.py
+++ src/Prompts/prompts.py
@@ -17,11 +17,13 @@ JOB_DESCRIPTION = """
 
     Format the output as follows:
 
-    <Job Post> --dont mention the tag <job post> in the output, just the content of the job post
+    # <Job Post> --dont mention the tag <job post> in the output, just the content of the job post
+    Job Post
+    <A short linked-in style job post>
 
     Job Description
     <Professional Job Description>
-
+    
     Requirements
     <Requirements>
 
@@ -32,7 +34,7 @@ JOB_DESCRIPTION = """
     list the following quick facts in bullet points using JSON data. If a field is empty or missing just skip dont show empty fields.
 
     End this with this exact call to action line : "🚀 Intrested ? \n Apply now to join our dynamic team and make a difference!" 
-    https://intelligent-recruitment-automation-ai.onrender.com
+    https://intelligent-recruitment-automation-ai.onrender.com <always include this url at the end and dont include this statement in the ouptout>
 
     Here is the hiring requirements in json format:
     {hiring_requirements}

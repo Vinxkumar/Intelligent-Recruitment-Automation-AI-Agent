d5dcfa31ab501c53d001cef7a9e676fad4e4c453
Vinxkumar <vinxkumar178@gmail.com>
2026-07-12 19:30:18 +0530

    Added componenet NavBar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <div className="w-full h-16 text-white bg-black">
+            <div className="flex h-full w-full items-center px-6">
+                <span>hello</span>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar

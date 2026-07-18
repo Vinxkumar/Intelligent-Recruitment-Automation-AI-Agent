574989afdd5ee627c462d0525f93eef7a21c19ec
Vinxkumar <vinxkumar178@gmail.com>
2026-07-18 17:46:45 +0530

    added new route for new job posting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-
 import './App.css'
 import { Route, Routes } from "react-router-dom"
-
+import NavBar from './components/Navbar'
 import Home from './pages/Home'
-
+import NewJobForm from './pages/NewJob'
 function App() {
-
-
   return (
-    <Routes>
+    <div className="w-full min-h-screen flex flex-col">
+      <header className="w-full h-16">
+        <NavBar />
+      </header> 
 
-      <Route path="/" element={<Home/>}/>
-
-    </Routes>
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-job" element={<NewJobForm/>} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 

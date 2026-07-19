d33538cdbecb9ea20de19f0ed91c68a47fe64ce6
Vinxkumar <vinxkumar178@gmail.com>
2026-07-19 17:01:37 +0530

    updated interface

--- src/types/Job.ts
+++ src/types/Job.ts
@@ -1,22 +1,18 @@
-
-interface TechStack {
-    name: string
-}
-
 export interface Job {
-    id?: number,
-    title: string,
-    companyName: string,
-    companyEmail: string,
-    pQualification: string,
-    mQualification?: string,
-    jd?:string,
-    techStack: TechStack[],
-    experience: "FRESHER" | "JOUINUER" | "INTERMEDIATE" | "SENIOR",
-    location?: string,
-    compensation?: string,
-    empType: "FULL_TIME" | "PART_TIME" | "INTERNSHIP" | "CONTRACT",
-    workType: "ON_SITE" |"REMOTE" | "HYBRID"
-    vacancies?: string
-    deadLine?:Date
+  id?: number;
+  title: string;
+  companyName: string;
+  companyEmail: string;
+  pQualification: string;
+  mQualification?: string;
+  jd?: string;
+  techStack: string[];              // was TechStack[]
+  experience: "FRESHER" | "JOUINUER" | "INTERMEDIATE" | "SENIOR";
+  location?: string;
+  compensation?: string;
+  empType: "FULL_TIME" | "PART_TIME" | "INTERNSHIP" | "CONTRACT";
+  workType: "ON_SITE" | "REMOTE" | "HYBRID";
+  vacancies?: string;
+  deadLine?: string | number; 
+  createdAt? :string | number;               // was Date — send as "YYYY-MM-DD" to match java.util.Date cleanly
 }

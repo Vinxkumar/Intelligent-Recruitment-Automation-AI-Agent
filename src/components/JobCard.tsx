3bc67c02d825add44aedd977d9a2ada675d142c2
Vinxkumar <vinxkumar178@gmail.com>
2026-07-21 14:31:46 +0530

    updated UI

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -18,37 +18,34 @@ import { MdWorkHistory } from "react-icons/md";
 import { BsFillSuitcaseLgFill } from "react-icons/bs";
 import { ImClock } from "react-icons/im";
 import { GrEmptyCircle } from "react-icons/gr";
-
+import { SiAgentskills } from "react-icons/si";
 
 const JobCard = ({
     id,
     title,
     companyName,
-    pQualification,
-    mQualification,
-    jd,
-    techStack,
-    experience,
     location,
     compensation,
     empType,
     workType,
     vacancies,
-    deadLine,
     createdAt,
+    expRange,
+    deadLine
 } : Job ) => {
 
     const date = createdAt? new Date(createdAt).toISOString().split("T")[0]: createdAt;
+    const ddate = deadLine? new Date(deadLine).toISOString().split("T")[0]: deadLine;
 
     return (
         <div className="w-full h-full hover:-translate-y-2 transition-all ease-in-out hover:z-10 ">
-            <Card key={id} className="groupcursor-pointer font-poppins w-full h-full bg-[#161b22] rounded-xl">
+            <Card key={id} className="groupcursor-pointer  font-poppins w-full h-full bg-[#161b22] ">
                 <CardHeader>
-                    <CardTitle className="text-xl text-[#e6edf3] font-poppins ">{title}</CardTitle>
-                    <CardDescription className="gap-2 text-[#8b949e]">
+                    <CardTitle className="text-xl text-[#e6edf3]  flex items-start font-poppins ">{title}</CardTitle>
+                    <CardDescription className="gap-2 text-[#8b949e]  flex items-start flex-col">
                          <p>{companyName}</p>
                                                  {createdAt && (
-                            <span className="flex items-center gap-1"><ImClock className="text-red-600"/> <p>{formatRelativeTime(date)}</p></span>
+                            <span className="flex items-center gap-1"><ImClock className="text-green-600"/> <p>{formatRelativeTime(date)}</p></span>
                         )}
                     </CardDescription>
                     <CardAction>
@@ -56,18 +53,27 @@ const JobCard = ({
                     </CardAction>
                 </CardHeader>
                 <CardContent className="flex flex-col">
-                    <span className="flex gap-1 text-xl text-[#e6edf3] items-center"><FaRupeeSign /> {compensation}/yr</span>
+                    <span className="flex  gap-1 text-xl text-[#e6edf3] items-center">
+                        <FaRupeeSign /> {compensation}/yr 
+                        
+                    </span>
                     <div className=" flex  gap-3 mt-[4%] text-md">
                         <span className="flex items-center hover:-translate-y-1 transition-all ease-in-out gap-1 bg-[#e6edf3]/30 rounded-md border-[#e6edf3]/50 border p-1"><MdLocationPin className="text-amber-500" /> <p>{location}</p></span>
                         <span className="flex items-center hover:-translate-y-1 transition-all ease-in-out gap-1 bg-[#e6edf3]/30 rounded-md border-[#e6edf3]/50 border p-1"><MdWorkHistory className="text-green-600"/> <p>{formatEnumLabel(workType)}</p></span>
                         <span className="flex items-center hover:-translate-y-1 transition-all ease-in-out gap-1 bg-[#e6edf3]/30 rounded-md border-[#e6edf3]/50 border p-1"><BsFillSuitcaseLgFill className="text-blue-700"/> <p>{formatEnumLabel(empType)}</p></span>
+
                         {vacancies && (
                             <span className="flex items-center hover:-translate-y-1 transition-all ease-in-out gap-1 bg-[#e6edf3]/30 rounded-md border-[#e6edf3]/50 border p-1"><GrEmptyCircle className="text-red-400"/> <p>{vacancies}</p></span>
                         )}
                     </div>
                 </CardContent>
-                <CardFooter>
-
+                <CardFooter className="gap-2 flex justify-between">
+                         <p className="flex gap-1 items-center text-md text-white"><SiAgentskills /> {formatEnumLabel(expRange)}</p>
+                        <span className="text-white items-center flex gap-1"> 
+                            <ImClock className="text-red-500"/>
+                            <p>Apply</p>
+                            {formatRelativeTime(ddate)}
+                        </span>
                 </CardFooter>
             </Card>
         </div>

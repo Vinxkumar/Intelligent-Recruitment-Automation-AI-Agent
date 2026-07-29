02b4b4160d8a36d97a4933831675694950d98beb
Vinxkumar <vinxkumar178@gmail.com>
2026-07-29 20:02:43 +0530

    implemented ApplyModal

--- src/pages/Opportunities.tsx
+++ src/pages/Opportunities.tsx
@@ -2,9 +2,7 @@ import getJobs from "@/services/getJobs";
 import { useEffect, useState } from "react";
 import type { Job } from "@/types/Job";
 import JobCard from "@/components/JobCard";
-
 import Loader from "../assets/ui/Loader";
-
 import JobViewModal from "@/components/Modal/JobViewModal";
 
 const Opportunities = () => {
@@ -29,7 +27,7 @@ const Opportunities = () => {
 
   if (loading) {
     return (
-      <div className="w-full h-full  bg-[#0d1117] flex items-center justify-center">
+      <div className="w-full h-full bg-[#0d1117] flex items-center justify-center">
         <div className="text-lg font-medium text-gray-500">
           <Loader />
         </div>
@@ -39,7 +37,7 @@ const Opportunities = () => {
 
   if (error) {
     return (
-      <div className="w-full h-full min-h-[50vh] bg-[#] flex items-center justify-center">
+      <div className="w-full h-full min-h-[50vh] flex items-center justify-center">
         <div className="text-red-600 text-lg font-medium">{error}</div>
       </div>
     );
@@ -54,14 +52,10 @@ const Opportunities = () => {
               key={jb.id}
               {...jb}
               trigger={
-                <div className="cursor-pointer ">
+                <div className="cursor-pointer">
                   <JobCard {...jb} />
                 </div>
               }
-              onApply={() => {
-                // wire this to whatever your apply flow ends up being
-                
-              }}
             />
           ))}
       </div>
